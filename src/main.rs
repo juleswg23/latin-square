752decdced8c86d283f8494f8804d0891ad82986
juleswg23 <jiw1@williams.edu>
2024-08-16 11:58:21 -0400

    Some more functionality on cubes

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,10 @@ impl LatinSolver {
     fn new(order: usize) -> LatinSolver {
         LatinSolver {
             order: order,
-            cube: vec![true; order.pow(3)],
+            cube: vec![true; order.pow(3)], // false when value is not a possibility in that square
             grid: vec![0; order.pow(2)],
-            col: vec![true; order.pow(2)],
-            row: vec![true; order.pow(2)],
+            row: vec![false; order.pow(2)], // set to true when the val is present in row x
+            col: vec![false; order.pow(2)], // set to true when the val is present in col y
         }
     }
 
@@ -32,6 +32,11 @@ impl LatinSolver {
         self.cube[position]
     }
 
+    fn set_cube_value(&mut self, x: usize, y: usize, n: usize) -> () {
+        let position = self.get_cube_pos(x, y, n);
+        self.cube[position] = false;
+    }
+
     fn get_grid_pos(&self, x: usize, y: usize) -> usize {
         x * &self.order + y
     }
@@ -40,6 +45,17 @@ impl LatinSolver {
         self.grid[self.get_grid_pos(x, y)]
     }
 
+    // fn update_cube(&self, x: usize, y:usize) -> () {
+    //     {}
+    // }
+
+    fn place_digit(&mut self, x: usize, y:usize, digit: usize) -> () {
+        for n in  1..=5 {
+            if n != digit { self.set_cube_value(x, y, n) }
+        }
+    }
+
+    
     
 
 
@@ -49,10 +65,14 @@ impl LatinSolver {
 
 
 fn main() {
-    let ls = LatinSolver::new(6);
+    let mut ls = LatinSolver::new(6);
     println!("{}", ls.cube.len());
     println!("{}", ls.get_cube_pos(3, 3, 2));
     println!("{}", ls.get_cube_value(3, 3, 2));
     println!("{}", ls.get_grid_value(3, 3));
+    ls.place_digit(3, 3, 5);
+    println!("{}", ls.get_cube_value(3, 3, 2));
+    println!("{}", ls.get_cube_value(3, 3, 5));
+
 
 }
