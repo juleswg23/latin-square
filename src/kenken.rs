df88309bfe08228a2aa424cb2231722bab6669ff
juleswg23 <jules.walzergoldfeld@gmail.com>
2024-09-03 15:14:06 -0400

    Moved math module

--- src/kenken.rs
+++ src/kenken.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 use crate::latin::LatinSolver;
 use core::cmp::min;
+use crate::math::math;
 
 // The possible operations on a clue for a KenKen
 #[derive(Clone, Debug, strum_macros::Display)]
@@ -21,28 +22,6 @@ struct Clue {
     target: usize,
 }
 
-// Some useful mathemtatic operations reused in a couple different contexts
-pub mod math {
-    // Given an index into a flat array, return the (x,y) index pair.
-    pub fn xy_pair(index: usize, order: usize) -> (usize, usize) {
-        assert!(index < order.pow(2), "The index provided to xy_pair is out of range.");
-        (index / order, index % order)
-    }
-
-    // Returns the location of the rightmost set bit
-    // Note that this is in digit notation, so 0b110 returns 2
-    pub fn min_bit(mask: i32) -> usize {
-        assert_ne!(mask, 0b0, "Trying to find min_bit but no bits are set.");
-        ((mask & -mask) as f32).log2() as usize + 1
-    }
-
-    // Returns the location of the leftmose set bit
-    // Note that this is in digit notation, so 0b110 returns 3
-    pub fn max_bit(mask: i32) -> usize {
-        assert_ne!(mask, 0b0, "Trying to find max_bit but no bits are set.");
-        (mask as f32 + 1.0).log2().ceil() as usize
-    }
-}
 
 // A data structure for each region of the ken ken puzzle.
 // It will not know about the contents of the cells, just the clue and locations.
