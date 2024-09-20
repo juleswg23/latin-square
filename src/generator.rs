2cafb26934af9c54f8098fbbc2125450d62f0d33
juleswg23 <jules.walzergoldfeld@gmail.com>
2024-09-20 14:11:06 -0400

    Generator working

--- src/generator.rs
+++ src/generator.rs
@@ -3,7 +3,7 @@ use crate::kenken::{Clue, KenKen, Operation, Region};
 use crate::latin::SolvedStatus;
 use rand::seq::SliceRandom;
 use rand::{thread_rng, Rng};
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::ops::Deref;
 use crate::kenken::kenken_solve::ken_ken_logical_solver_with_grid;
 use crate::latin::latin_solve::stepped_logical_solver;
@@ -38,7 +38,7 @@ fn insert_random_region(ken_ken: &mut KenKen) -> Option<()> {
 }
 
 // Check if two cells are adjacent
-fn is_adjacent(order: usize, first: usize, second: usize) -> bool {
+fn is_adjacent(order: usize, first: &usize, second: &usize) -> bool {
     assert!(order > 1, "A grid must be at least 2x2");
 
     let first_x = first / order;
@@ -52,6 +52,7 @@ fn is_adjacent(order: usize, first: usize, second: usize) -> bool {
     (first_x.abs_diff(second_x) + first_y.abs_diff(second_y)) == 1
 }
 
+/// Return a vector of all the cells not attributed to a region in a kenken
 fn get_empty_cells(ken_ken: &KenKen) -> Vec<usize> {
     let vec: Vec<usize> = (0..ken_ken.order() * ken_ken.order()).collect();
     let mut result: HashMap<usize, bool> = vec.into_iter().map(|x| (x, true)).collect::<HashMap<_, _>>();
@@ -124,7 +125,8 @@ fn create_puzzle_from_grid(grid: &mut Grid) -> KenKen {
     let mut ken_ken = KenKen::new(grid.order());
     
     while get_empty_cells(&ken_ken).len() != 0 {
-        let new_cells: Vec<usize> = find_contiguous_region(&ken_ken);
+        let region_size = weighted_random(get_empty_cells(&ken_ken).len());
+        let new_cells: Vec<usize> = find_contiguous_region(&ken_ken, region_size);
 
         // randomly choose operation
         let ops_vec = match new_cells.len() {
@@ -152,13 +154,55 @@ fn create_puzzle_from_grid(grid: &mut Grid) -> KenKen {
     
 }
 
-// 
-fn find_contiguous_region(ken_ken: &KenKen) -> Vec<usize> {
+// O(order^2 * region_size) implementation of getting a contiguous region from the kenken grid
+/// Returns a vector of contiguous squares 
+fn find_contiguous_region(ken_ken: &KenKen, region_size: usize) -> Vec<usize> {
     let mut empty_cells = get_empty_cells(ken_ken);
+    // let original_len = empty_cells.len();
+    assert!(empty_cells.len() > 0, "Should never have no empty cells and call this function");
+    assert!(region_size > 0, "Cannot have a region of size 0");
+
     empty_cells.shuffle(&mut thread_rng()); // TODO Problem is they are not contiguous
-    let region_size = weighted_random(empty_cells.len());
-    
-    todo!()
+    let mut region_size = region_size; // TODO maybe different way of finding region size
+
+    let mut contiguous_region: Vec<usize> = Vec::new();
+    let mut empty_cells: VecDeque<usize> = VecDeque::from(empty_cells);
+    contiguous_region.push(empty_cells.pop_front().unwrap()); // can't panic because of assert
+    // empty_cells.push_back(ken_ken.order().pow(3)); // This is our end of queue value
+
+    region_size -= 1;
+    let mut cycles_without_reset = 0;
+
+    while region_size > 0 && cycles_without_reset <= empty_cells.len() + 1 && empty_cells.len() > 0 {
+        let empty_cell = empty_cells.pop_front().unwrap();
+        let mut to_insert = None;
+
+        // check all the cells in the contiguous region to see if this would be contiguous
+        for used_cell in &contiguous_region {
+            if is_adjacent(ken_ken.order(), &empty_cell, used_cell) {
+                to_insert = Some(empty_cell);
+                break;
+            }
+        }
+
+        // check if we found a cell to insert
+        match to_insert {
+            Some(cell) => {
+                cycles_without_reset = 0;
+                region_size -= 1;
+                contiguous_region.push(cell);
+            },
+            None => {
+                cycles_without_reset += 1;
+                empty_cells.push_back(empty_cell);
+            },
+        }
+    }
+
+    assert_eq!(0, cycles_without_reset, "A full region was not found, based on cycles_without_reset");
+    assert_eq!(0, region_size, "A full region was not found, based on region_size");
+
+    contiguous_region
 }
 
 // Takes a grid, and reports if it's solved or now
@@ -191,12 +235,12 @@ mod tests {
 
     #[test]
     fn test_is_adjacent() {
-        assert!(is_adjacent(3, 4, 3));
-        assert!(!is_adjacent(3, 2, 3));
-        assert!(is_adjacent(3, 2, 5));
-        assert!(!is_adjacent(3, 2, 2));
-        assert!(!is_adjacent(2, 1, 2));
-        assert!(!is_adjacent(2, 0, 3));
+        assert!(is_adjacent(3, &4, &3));
+        assert!(!is_adjacent(3, &2, &3));
+        assert!(is_adjacent(3, &2, &5));
+        assert!(!is_adjacent(3, &2, &2));
+        assert!(!is_adjacent(2, &1, &2));
+        assert!(!is_adjacent(2, &0, &3));
     }
 
 
@@ -210,6 +254,52 @@ mod tests {
         
         // println!("{}", grid.candidates_to_string());
         // println!("{}", grid.digits_to_string());
+    }
+
+    #[test]
+    fn find_region_one_cell() {
+        let mut k = KenKen::new(1);
+        let region = find_contiguous_region(&k, 1);
+        assert_eq!(vec![0], region);
+    }
+    #[test]
+    fn find_region_many_cells() {
+        let mut k = KenKen::new(2);
+        let mut region = find_contiguous_region(&k, 4);
+        region.sort();
+        assert_eq!(vec![0, 1, 2, 3], region);
+
+        let mut k = KenKen::new(3);
+        let mut region = find_contiguous_region(&k, 9);
+        region.sort();
+        assert_eq!(vec![0, 1, 2, 3, 4, 5, 6, 7, 8], region);
+    }
+
+    #[test]
+    fn find_region_finds_contiguous() {
+        let mut k = KenKen::new(6);
+        let mut region = find_contiguous_region(&k, 6);
+        for i in 0..region.len() {
+            let mut is_adjacent_to_something = false;
+            for j in 0..region.len() {
+                if i != j {
+                    is_adjacent_to_something |= is_adjacent(k.order(), &region[i], &region[j]);
+                }
+            }
+            assert!(is_adjacent_to_something, "{:?}", region);
+        }
+
+        let mut k = KenKen::new(10);
+        let mut region = find_contiguous_region(&k, 9);
+        for i in 0..region.len() {
+            let mut is_adjacent_to_something = false;
+            for j in 0..region.len() {
+                if i != j {
+                    is_adjacent_to_something |= is_adjacent(k.order(), &region[i], &region[j]);
+                }
+            }
+            assert!(is_adjacent_to_something, "{:?}", region);
+        }
     }
 
 
